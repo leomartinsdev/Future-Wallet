781c5df76465e67b7cd33eed3f0d1a87b9d79ee6
Leonardo Martins <leonardogustavo@outlook.com>
2023-05-15 20:19:32 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
-} // oi
+  return (
+    <main>
+      <span>Trybewallet</span>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
+      </Switch>
+    </main>
+  );
+}
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import validator from 'validator';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { user } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    loginVerifications: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    }, this.validations);
+  };
+
+  validations = () => {
+    this.loginVerifications();
+  };
+
+  loginVerifications = () => {
+    const { password, email } = this.state;
+    const minPassChars = 6;
+    const validPassword = password.length >= minPassChars;
+    const validEmail = validator.isEmail(email);
+
+    this.setState({
+      loginVerifications: !(validPassword && validEmail),
+    });
+  };
+
+  handleEnterBtn = (email) => {
+    const { history, dispatch } = this.props;
+    dispatch(user(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { loginVerifications, email, password } = this.state;
+    return (
+      <div>
+        <span>Login</span>
+        <section>
+          <label htmlFor="email">
+            <input
+              name="email"
+              type="email"
+              placeholder="email@email.com"
+              data-testid="email-input"
+              value={ email }
+              onChange={ this.onInputChange }
+            />
+          </label>
+
+          <label htmlFor="password">
+            <input
+              name="password"
+              type="password"
+              placeholder="Senha"
+              data-testid="password-input"
+              value={ password }
+              onChange={ this.onInputChange }
+            />
+          </label>
+
+          <button
+            type="button"
+            disabled={ loginVerifications }
+            onClick={ () => this.handleEnterBtn(email) }
+          >
+            Entrar
+          </button>
+        </section>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = ({
+  dispatch: PropTypes.any,
+}).isRequired;
+
+export default connect()(Login);
